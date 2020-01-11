c0cdb249b4f2da0d91810d88f7c137fbc5fae6ec
Luis camilo <lhenriquegomescamilo@gmail.com>
2020-01-11 15:02:44 -0300

    Criando modulo de estrutura inicial do front-end

--- wallet-tracking-web/src/app/components/sidebar/sidebar.component.ts
+++ wallet-tracking-web/src/app/components/sidebar/sidebar.component.ts
@@ -7,6 +7,7 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
+  { path: '/stock-market', title: 'Ações',  icon: 'design_app', class: '' },
     { path: '/dashboard', title: 'Dashboard',  icon: 'design_app', class: '' },
     { path: '/icons', title: 'Icons',  icon:'education_atom', class: '' },
     { path: '/maps', title: 'Maps',  icon:'location_map-big', class: '' },
---
+++ wallet-tracking-web/src/app/layouts/stocket-market/stocket-market.routing.ts
@@ -0,0 +1,6 @@
+import {Routes} from '@angular/router';
+import {StockerMarketMainComponent} from './stocker-market-main/stocker-market-main.component';
+
+export const StockerMarketRoutes: Routes = [
+  {path: 'main', component: StockerMarketMainComponent}
+];
