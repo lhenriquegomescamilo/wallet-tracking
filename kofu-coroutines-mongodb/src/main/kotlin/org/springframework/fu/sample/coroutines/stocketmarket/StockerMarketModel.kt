8f5bb1a4be83ba7f2bc755777637bd279a236278
Luis camilo <lhenriquegomescamilo@gmail.com>
2020-01-11 21:27:42 -0300

    Criando modelo para tracking das ações

--- kofu-coroutines-mongodb/src/main/kotlin/org/springframework/fu/sample/coroutines/stocketmarket/StockerMarketModel.kt
+++ kofu-coroutines-mongodb/src/main/kotlin/org/springframework/fu/sample/coroutines/stocketmarket/StockerMarketModel.kt
@@ -2,8 +2,23 @@ package org.springframework.fu.sample.coroutines.stocketmarket
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.util.*
 
 @Document
-data class StockerMarketModel (
-    @Id val code: String
+data class StockerMarketModel(
+    @Id val code: String,
+    val quantidade: Long,
+    val preco: Long,
+    val dataCompra: Date,
+    val historicoAcoes: List<HistoricoAcao> = emptyList()
+)
+
+data class HistoricoAcao(
+    @Id val codigoAcao: String,
+    val datadoDia: Date,
+    val valorAbertura: Long,
+    val valorMaxima: Long,
+    val valorMinima: Long,
+    val valorFechamento: Long
+
 )
